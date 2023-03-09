587ca052cb931a8836075ef3641229fe370b5621
Balaji <59250390+scamanderoffl@users.noreply.github.com>
2023-03-09 20:45:57 +0530

    Add files via upload

---
+++ src/BLINK_BUILTIN_LED.c
@@ -0,0 +1,22 @@
+/*****************************************************************
+
+	Title	:	Blink Builtin LED
+	MCU	:	ATMEGA328P
+	Author	: 	scamanderoffl
+	
+*****************************************************************/
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    DDRB=(1<<PB5);		 //set PORTB PIN 5 as output
+    while (1)
+    {
+        PORTB=(1<<PB5);		//set PIN 5 as HIGH
+        _delay_ms(100);
+        PORTB=0;	       //set PIN 5 as LOW
+        _delay_ms(100);
+    }
+    
+}
